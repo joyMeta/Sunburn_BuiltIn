f2711928c33d06509acafc07528be7e101168a75
Joy Goldar <joy@metastudios.gg>
2023-06-16 17:35:37 +0530

    Remove audio codecs

--- Assets/Scripts/Audio/NetworkAudioManager.cs
+++ Assets/Scripts/Audio/NetworkAudioManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Photon.Pun;
+using System.IO;
 
 public class NetworkAudioManager : MonoBehaviour {
 
@@ -12,15 +13,17 @@ public class NetworkAudioManager : MonoBehaviour {
     }
 
     public void PlayAudio(AudioClip audioClip,bool play) {
-        Debug.Log(audioClip.name);
-        byte[] clipBytes=Utilities.WavUtility.FromAudioClip(audioClip);
+        byte[] clipBytes=File.ReadAllBytes(audioClip.name);
         photonView.RPC("RPC_PlayAudio", RpcTarget.AllBuffered, clipBytes, audioClip.name,play);
     }
 
     [PunRPC]
     private void RPC_PlayAudio(byte[] audioData,string fileName,bool play) {
         print(fileName);
-        audioSource.clip=Utilities.WavUtility.ToAudioClip(audioData,0,fileName);
+        using (FileStream bytoToAudio=File.Create(fileName)) {
+            bytoToAudio.Write(audioData,0,audioData.Length);
+        }
+        audioSource.clip= Utilities.WavUtility.ToAudioClip(audioData,0,fileName);
         if (play)
             audioSource.Play();
         else
