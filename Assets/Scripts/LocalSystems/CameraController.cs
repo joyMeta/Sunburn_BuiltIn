06a1c796719ad28a98d12ba9c4710cd55a22d20b
Joy Goldar <joy@metastudios.gg>
2023-06-23 11:26:18 +0530

    Camera Update

--- Assets/Scripts/LocalSystems/CameraController.cs
+++ Assets/Scripts/LocalSystems/CameraController.cs
@@ -22,8 +22,6 @@ public class CameraController : MonoBehaviour {
     void Start() {
         cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
         input = GetComponent<PlayerInput>();
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void Update() {
